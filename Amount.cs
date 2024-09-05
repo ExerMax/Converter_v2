be9f7178e54b28107b3899419bc8b96b2ab7dca8
ExerMax <145705503+ExerMax@users.noreply.github.com>
2024-09-06 00:21:41 +0400

    Publish

---
+++ Amount.cs
@@ -0,0 +1,28 @@
+﻿namespace Converter_v2
+{
+    public struct Amount
+    {
+        public decimal Value { get; set; }
+        public string Name { get; set; }
+
+        public static Amount operator +(Amount a1, Amount a2)
+        {
+            if (a1.Name != a2.Name)
+            {
+                throw new ArgumentException("Different currencies");
+            }
+
+            return new Amount { Value = a1.Value + a2.Value, Name = a1.Name };
+        }
+
+        public static Amount operator -(Amount a1, Amount a2)
+        {
+            if (a1.Name != a2.Name)
+            {
+                throw new ArgumentException("Different currencies");
+            }
+
+            return new Amount { Value = a1.Value - a2.Value, Name = a1.Name };
+        }
+    }
+}
---
+++ IConverter.cs
@@ -0,0 +1,7 @@
+﻿namespace Converter_v2
+{
+    public interface IConverter
+    {
+        Amount Convert(Amount value, string currency);
+    }
+}
